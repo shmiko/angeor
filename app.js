ca47a42892a879400079de8b39e4f0dadb8e090e
Paul Jones <shmiko@gmail.com>
2015-09-20 18:47:47 +1000

    angeor 1.2.2 controller as syntax

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ Parties = new Mongo.Collection("parties");
 
 if (Meteor.isClient) {
   angular.module('angeor',['angular-meteor']);
-  angular.module('angeor').controller('PartiesListCtrl', ['$scope', '$meteor', function ($scope,$meteor) {
+  angular.module('angeor')
+      .controller('PartiesListCtrl', ['$meteor', function ($meteor) {
+
+          var vm = this;
     //$scope.parties = [
     //  {
     //    'name': 'Dubstep-Free Zone',
@@ -22,7 +25,16 @@ if (Meteor.isClient) {
     //  }
     //];
 
-    $scope.parties = $meteor.collection(Parties);
+          vm.parties = $meteor.collection(Parties);
+
+          vm.remove = function(party){
+          //$scope.parties.splice($scope.parties.indexOf(party), 1);
+          vm.parties.remove(party);
+      };
+
+          vm.removeAll = function(){
+          vm.parties.remove();
+      };
   }]);
 }
 
