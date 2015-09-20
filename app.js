edfe4beeba2c6f22207d3e56b99666c3f1b1ba09
Paul Jones <shmiko@gmail.com>
2015-09-20 18:19:00 +1000

    angeor 1.2 form with mongo

--- app.js
+++ app.js
@@ -1,9 +1,44 @@
+Parties = new Mongo.Collection("parties");
+
 if (Meteor.isClient) {
   angular.module('angeor',['angular-meteor']);
+  angular.module('angeor').controller('PartiesListCtrl', ['$scope', '$meteor', function ($scope,$meteor) {
+    //$scope.parties = [
+    //  {
+    //    'name': 'Dubstep-Free Zone',
+    //    'description': 'Can we please just for an evening not listen to dubstep.'
+    //  },
+    //  {
+    //    'name': 'All dubstep all the time',
+    //    'description': 'Get it on!'
+    //  },
+    //  {
+    //    'name': 'Savage lounging',
+    //    'description': 'Leisure suit required. And only fiercest manners.'
+    //  },
+    //  {
+    //    'name': 'All mixymix',
+    //    'description': 'Get up!'
+    //  }
+    //];
+
+    $scope.parties = $meteor.collection(Parties);
+  }]);
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    if (Parties.find().count() === 0) {
+      var parties = [
+        {'name': 'Dubstep-Free Zone',
+          'description': 'Fast just got faster with Nexus S.'},
+        {'name': 'All dubstep all the time',
+          'description': 'Get it on!'},
+        {'name': 'Savage lounging',
+          'description': 'Leisure suit required. And only fiercest manners.'}
+      ];
+      for (var i = 0; i < parties.length; i++)
+        Parties.insert(parties[i]);
+    }
   });
 }
