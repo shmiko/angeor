2f7a9e71b30eeefa152f01b4991a5f534ec24f93
Paul Jones <shmiko@gmail.com>
2015-09-24 08:21:56 +1000

    Adding user accounts

--- model/parties.js
+++ model/parties.js
@@ -2,3 +2,14 @@
  * Created by pauljones on 23/09/15.
  */
 Parties = new Mongo.Collection("parties");
+Parties.allow({
+    insert: function (userId, party) {
+        return userId && party.owner === userId;
+    },
+    update: function (userId, party, fields, modifier) {
+        return userId && party.owner === userId;
+    },
+    remove: function (userId, party) {
+        return userId && party.owner === userId;
+    }
+});
