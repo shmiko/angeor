e7efd5cea2ce9cb5eb30ca9c1a9a801fa6640016
Paul Jones <shmiko@gmail.com>
2015-09-24 17:30:38 +1000

    Adding user accounts.

--- client/routes.js
+++ client/routes.js
@@ -1,6 +1,16 @@
 /**
  * Created by pauljones on 23/09/15.
  */
+angular.module("angeor").run(["$rootScope", "$location", function($rootScope, $state) {
+    $rootScope.$on("$stateChangeError", function(event, next, previous, error) {
+        // We can catch the error thrown when the $requireUser promise is rejected
+        // and redirect the user back to the main page
+        if (error === "AUTH_REQUIRED") {
+            $state.go("/parties");
+        }
+    });
+}]);
+
 angular.module('angeor').config(['$urlRouterProvider', '$stateProvider', '$locationProvider',
     function($urlRouterProvider, $stateProvider, $locationProvider){
 
