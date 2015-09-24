e617500a156c74c67a567ea37736d915a16b75fb
Paul Jones <shmiko@gmail.com>
2015-09-25 08:05:38 +1000

    pagination.

--- client/parties/controllers/partiesList.js
+++ client/parties/controllers/partiesList.js
@@ -1,7 +1,19 @@
 angular.module("socially").controller("PartiesListCtrl", ['$scope', '$meteor',
   function($scope, $meteor){
+    $scope.page = 1;
+    $scope.perPage = 3;
+    $scope.sort = { name: 1 };
 
-    $scope.parties = $meteor.collection(Parties).subscribe('parties');
+    $scope.parties = $meteor.collection(function() {
+      return Parties.find({}, {
+        sort : $scope.sort
+      });
+    });
+    $meteor.subscribe('parties', {
+      limit: parseInt($scope.perPage),
+      skip: parseInt(($scope.page - 1) * $scope.perPage),
+      sort: $scope.sort
+    });
 
     $scope.remove = function(party){
       $scope.parties.splice( $scope.parties.indexOf(party), 1 );
