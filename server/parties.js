7effda44889468c69d7a347c4480147dd83842f8
Paul Jones <shmiko@gmail.com>
2015-09-25 08:09:49 +1000

    pagination...

--- server/parties.js
+++ server/parties.js
@@ -2,6 +2,17 @@
  * Created by pauljones on 24/09/15.
  */
 Meteor.publish("parties", function (options) {
+    Counts.publish(this, 'numberOfParties', Parties.find({
+        $or:[
+            {$and:[
+                {'public': true},
+                {'public': {$exists: true}}
+            ]},
+            {$and:[
+                {owner: this.userId},
+                {owner: {$exists: true}}
+            ]}
+        ]}), { noReady: true });
     return Parties.find({
         $or:[
             {$and:[
